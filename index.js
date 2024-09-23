4738e5f581a7e75552870485b83c9cf80d583321
Nortey-Micheal <norteymicheal91@gmail.com>
2024-09-23 01:47:47 -0700

    "Moved LinkedList implementation and test code from main.js to index.js, removing main.js"

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports.LinkedList = function () {
+function LinkedList () {
     this.value = 'head';
     this.nextNode = null;
     const append = (value) => {
@@ -159,3 +159,39 @@ function Node (value = null,next = null) {
     this.nextNode = next;
 }
 
+let list = new LinkedList();
+list.append('Hello1');
+list.append('Hello2');
+list.append('Hello3');
+list.append('Hello4');
+
+console.log(list)
+console.log(list.toString())
+
+list.prepend('Hello5');
+list.append('Hello6');
+list.prepend('Hello7');
+
+console.log(list)
+console.log(list.toString())
+
+console.log(list.size())
+console.log(list.head())
+console.log(list.tail())
+console.log(list.at(3))
+
+list.pop();
+console.log(list.toString())
+console.log(list.size())
+
+list.pop();
+list.insertAt('what',0)
+console.log(list.toString())
+console.log(list.size())
+
+console.log(list.contains('Hello7'))
+
+console.log(list.find('Hello2'))
+
+list.removeAt(2)
+console.log(list.toString())
